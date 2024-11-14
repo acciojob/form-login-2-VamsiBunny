c65326f8919c95b248623f7fbe63ff868be380ba
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-14 20:41:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+let mybtn=document.getElementById("btn");
+let fname=document.getElementById("f-name");
+let lname=document.getElementById("l-name");
+let email=document.getElementById("mail");
+let number=document.getElementById("num");
+
+function submit() {
+	console.log("Full Name:",fname.value);
+	console.log("Last Name:",lname.value);
+	console.log("Phone Number:",number.value);
+	console.log("Email:",email.value);
+}
